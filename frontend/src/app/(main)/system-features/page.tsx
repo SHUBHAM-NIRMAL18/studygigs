1ec2161f682fe0e655163ee7aae5330085759ef4
Shubham Nirmal <suvamniraml456@gmail.com>
2026-07-12 22:00:08 +0545

    feat: add authenticated route for features page under /system-features

---
+++ frontend/src/app/(main)/system-features/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { FeaturesView } from '@/components/studygig/FeaturesView'
+
+export default function AuthenticatedFeaturesPage() {
+  return (
+    <div className="w-full py-2">
+      <FeaturesView />
+    </div>
+  )
+}
